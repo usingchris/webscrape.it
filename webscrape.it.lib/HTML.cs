6859271df4e8508eeee6ebd1d3d6c9422de38558
Chris Rowe <usingchris@gmail.com>
2012-04-08 09:32:31 +0200

    Sunday check in

---
+++ webscrape.it.lib/HTML.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using webscrape.it.lib.Model;
+
+namespace webscrape.it.lib
+{
+    public class HTML
+    {
+        public List<TableData> GetTableDatas(string source)
+        {
+            List<TableData> tabledatas = new List<TableData>();
+
+            //INSERT TDS HERE
+
+            return tabledatas;
+        }
+    }
+}
---
+++ webscrape.it.lib/Model/TableData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace webscrape.it.lib.Model
+{
+    public class TableData
+    {
+        public int Order { get; set; }
+        public string Content { get; set; }
+    }
+}
